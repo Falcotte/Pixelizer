17e3b59327680111fe3627d904b97532350c7aa0
Ali Unal <0033484@gmail.com>
2025-08-20 00:40:58 +0300

    Texturization optimized

--- Assets/Pixelization/Texturizer/Scripts/Texturizer.cs
+++ Assets/Pixelization/Texturizer/Scripts/Texturizer.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using Unity.Burst;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Jobs;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -25,11 +26,8 @@ namespace AngryKoala.Pixelization
 
         [SerializeField] private string _textureSavePath;
 
-        // Persistent buffers
         private NativeArray<Color32> _sourceNativeArray;
-        private NativeArray<Color32> _destinationNativeArray;
 
-        // Cached sizes to prevent unnecessary reallocations
         private int _cachedSourceWidth = -1;
         private int _cachedSourceHeight = -1;
         private int _cachedPixSize = -1;
@@ -92,11 +90,6 @@ namespace AngryKoala.Pixelization
             System.Diagnostics.Stopwatch stopwatch = System.Diagnostics.Stopwatch.StartNew();
 #endif
 
-            if (_newTexture != null)
-            {
-                DestroyImmediate(_newTexture);
-            }
-
             BuildTexture();
 
             TexturizedTexture = _newTexture;
@@ -115,33 +108,27 @@ namespace AngryKoala.Pixelization
 
             EnsureResources(sourceWidth, sourceHeight, pixSize);
 
-            if (!_sourceNativeArray.IsCreated || !_destinationNativeArray.IsCreated || _newTexture == null)
-            {
-                return;
-            }
-
-            if (sourceWidth == 0 || sourceHeight == 0)
+            if (!_sourceNativeArray.IsCreated || _newTexture == null || sourceWidth == 0 || sourceHeight == 0)
             {
                 return;
             }
 
             FillSourceFromPixCollection();
 
-            var buildTextureJob = new BuildTextureJob
+            var rawTextureData = _newTexture.GetRawTextureData<Color32>();
+
+            var buildTextureJob = new BuildTextureUnsafeJob
             {
                 Source = _sourceNativeArray,
-                Destination = _destinationNativeArray,
+                Destination = rawTextureData,
                 SourceWidth = sourceWidth,
                 SourceHeight = sourceHeight,
                 DestinationWidth = _cachedDestinationWidth,
                 PixSize = pixSize
             };
 
-            JobHandle jobHandle = buildTextureJob.Schedule(sourceWidth, 64);
+            JobHandle jobHandle = buildTextureJob.Schedule(sourceHeight, 1);
             jobHandle.Complete();
-
-            var rawTextureData = _newTexture.GetRawTextureData<Color32>();
-            rawTextureData.CopyFrom(_destinationNativeArray);
 
             _newTexture.Apply(updateMipmaps: false, makeNoLongerReadable: false);
         }
@@ -157,9 +144,6 @@ namespace AngryKoala.Pixelization
             int destinationHeight = sourceHeight * pixSize;
 
             bool sourceSizeChanged = (sourceWidth != _cachedSourceWidth) || (sourceHeight != _cachedSourceHeight);
-            bool pixSizeChanged = (pixSize != _cachedPixSize);
-            bool destinationSizeChanged = (destinationWidth != _cachedDestinationWidth) ||
-                                          (destinationHeight != _cachedDestinationHeight);
 
             if (sourceSizeChanged || !_sourceNativeArray.IsCreated)
             {
@@ -174,36 +158,25 @@ namespace AngryKoala.Pixelization
                         NativeArrayOptions.UninitializedMemory);
                 }
             }
-
-            if (destinationSizeChanged || !_destinationNativeArray.IsCreated)
-            {
-                if (_destinationNativeArray.IsCreated)
-                {
-                    _destinationNativeArray.Dispose();
-                }
-
-                if (destinationWidth > 0 && destinationHeight > 0)
-                {
-                    _destinationNativeArray = new NativeArray<Color32>(destinationWidth * destinationHeight,
-                        Allocator.Persistent, NativeArrayOptions.UninitializedMemory);
-                }
-            }
-
+            
             if (_newTexture == null || _newTexture.width != destinationWidth ||
                 _newTexture.height != destinationHeight || _newTexture.format != TextureFormat.RGBA32)
             {
                 if (destinationWidth > 0 && destinationHeight > 0)
                 {
-                    if (_newTexture != null)
+                    if (_newTexture == null)
                     {
-                        Destroy(_newTexture);
+                        _newTexture = new Texture2D(destinationWidth, destinationHeight, TextureFormat.RGBA32,
+                            false, false);
+                        _newTexture.filterMode = FilterMode.Point;
                     }
-
-                    _newTexture = new Texture2D(destinationWidth, destinationHeight, TextureFormat.RGBA32,
-                        mipChain: false, linear: false)
+                    else if (_newTexture.width != destinationWidth ||
+                             _newTexture.height != destinationHeight ||
+                             _newTexture.format != TextureFormat.RGBA32)
                     {
-                        filterMode = FilterMode.Point
-                    };
+                        _newTexture.Reinitialize(destinationWidth, destinationHeight, TextureFormat.RGBA32, false);
+                        _newTexture.filterMode = FilterMode.Point;
+                    }
                 }
             }
 
@@ -213,7 +186,7 @@ namespace AngryKoala.Pixelization
             _cachedDestinationWidth = destinationWidth;
             _cachedDestinationHeight = destinationHeight;
         }
-        
+
         private void FillSourceFromPixCollection()
         {
             int totalSourcePixels = _cachedSourceWidth * _cachedSourceHeight;
@@ -221,33 +194,24 @@ namespace AngryKoala.Pixelization
 
             int copyCount = Mathf.Min(totalSourcePixels, pixelCollection.Length);
 
-            int linearIndex = 0;
-            for (int i = 0; i < _cachedSourceWidth; i++)
+            for (int i = 0; i < copyCount; i++)
             {
-                for (int j = 0; j < _cachedSourceHeight; j++, linearIndex++)
-                {
-                    if (linearIndex < copyCount)
-                    {
-                        Color color = pixelCollection[linearIndex].Color;
-                        _sourceNativeArray[linearIndex] = new Color32(
-                            (byte)(Mathf.Clamp01(color.r) * 255f),
-                            (byte)(Mathf.Clamp01(color.g) * 255f),
-                            (byte)(Mathf.Clamp01(color.b) * 255f),
-                            255);
-                    }
-                    else
-                    {
-                        _sourceNativeArray[linearIndex] = new Color32(0, 0, 0, 255);
-                    }
-                }
+                Color32 color = pixelCollection[i].Color;
+                color.a = 255;
+
+                _sourceNativeArray[i] = color;
+            }
+
+            for (int i = copyCount; i < totalSourcePixels; i++)
+            {
+                _sourceNativeArray[i] = new Color32(0, 0, 0, 255);
             }
         }
 
-        [BurstCompile(FloatMode = FloatMode.Fast, CompileSynchronously = true)]
-        private struct BuildTextureJob : IJobParallelFor
+        [BurstCompile(FloatMode = FloatMode.Fast, FloatPrecision = FloatPrecision.Low, CompileSynchronously = true)]
+        private unsafe struct BuildTextureUnsafeJob : IJobParallelFor
         {
             [ReadOnly] public NativeArray<Color32> Source;
-
             [NativeDisableParallelForRestriction] public NativeArray<Color32> Destination;
 
             public int SourceWidth;
@@ -255,24 +219,38 @@ namespace AngryKoala.Pixelization
             public int DestinationWidth;
             public int PixSize;
 
-            public void Execute(int sourceColumnIndex)
+            public void Execute(int sourceRowIndex)
             {
-                int blockStartX = sourceColumnIndex * PixSize;
+                byte* destinationBase = (byte*)Destination.GetUnsafePtr();
+                Color32* sourceBase = (Color32*)Source.GetUnsafeReadOnlyPtr();
 
-                for (int sourceRowIndex = 0; sourceRowIndex < SourceHeight; sourceRowIndex++)
+                int bytesPerPixel = UnsafeUtility.SizeOf<Color32>();
+                long destinationRowStrideBytes = (long)DestinationWidth * bytesPerPixel;
+                
+                int destinationRowTop = (sourceRowIndex * PixSize) * DestinationWidth;
+
+                for (int sourceColumnIndex = 0; sourceColumnIndex < SourceWidth; sourceColumnIndex++)
                 {
-                    int sourceIndex = (sourceRowIndex * SourceWidth) + sourceColumnIndex;
-                    Color32 color = Source[sourceIndex];
-                    int blockStartY = sourceRowIndex * PixSize;
+                    int sourceIndex = sourceRowIndex * SourceWidth + sourceColumnIndex;
+                    Color32 color = sourceBase[sourceIndex];
 
-                    for (int offsetY = 0; offsetY < PixSize; offsetY++)
+                    int destinationColumnStart = sourceColumnIndex * PixSize;
+                    int runStart = destinationRowTop + destinationColumnStart;
+                    
+                    byte* firstRowPtr = destinationBase + (long)runStart * bytesPerPixel;
+                    
+                    UnsafeUtility.MemCpyReplicate(
+                        destination: firstRowPtr,
+                        source: &color,
+                        size: bytesPerPixel,
+                        count: PixSize
+                    );
+                    
+                    long rowBytes = (long)PixSize * bytesPerPixel;
+                    for (int y = 1; y < PixSize; y++)
                     {
-                        int destinationRowStart = ((blockStartY + offsetY) * DestinationWidth) + blockStartX;
-
-                        for (int offsetX = 0; offsetX < PixSize; offsetX++)
-                        {
-                            Destination[destinationRowStart + offsetX] = color;
-                        }
+                        byte* destinationRowPtr = firstRowPtr + y * destinationRowStrideBytes;
+                        UnsafeUtility.MemCpy(destinationRowPtr, firstRowPtr, rowBytes);
                     }
                 }
             }
@@ -284,17 +262,13 @@ namespace AngryKoala.Pixelization
             {
                 _sourceNativeArray.Dispose();
             }
-
-            if (_destinationNativeArray.IsCreated)
-            {
-                _destinationNativeArray.Dispose();
-            }
         }
 
         #endregion
-        
+
         public void SaveTexture()
         {
+#if UNITY_EDITOR
             if (!AssetDatabase.IsValidFolder(_textureSavePath))
             {
                 if (!CreateFolderAtSavePath(_textureSavePath))
@@ -336,10 +310,14 @@ namespace AngryKoala.Pixelization
             stopwatch.Stop();
             Debug.Log($"SaveTexture took {stopwatch.ElapsedMilliseconds} ms");
 #endif
+#else
+            Debug.LogWarning("SaveTexture is editor-only.");
+#endif
         }
-        
+
         private bool CreateFolderAtSavePath(string savePath)
         {
+#if UNITY_EDITOR
             if (string.IsNullOrWhiteSpace(savePath))
             {
                 Debug.LogError("Save path is null or empty");
@@ -372,6 +350,10 @@ namespace AngryKoala.Pixelization
             }
 
             return true;
+#else
+            Debug.LogWarning("CreateFolderAtSavePath is editor-only.");
+            return false;
+#endif
         }
     }
 }
